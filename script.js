606fc16d5d74721392b671ee3d7fc9cc5bf8403f
Andrew Siegel <siegelal7@gmail.com>
2020-09-14 13:28:18 -0400

    started javascript nonsense

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var apiKey = "RUN0H2sWaFCGb2vhcbToOvKXcrDt3l8R";
-console.log("test");
+$(document).ready(function () {
+  var apiKey = "RUN0H2sWaFCGb2vhcbToOvKXcrDt3l8R";
+  // console.log("test");
+  var inputValue = $("#keyword");
+  var numberOfArticles = $("#number-of-articles");
+  var startYear = $("#start-year");
+  var endYear = $("#end-year");
+  var searchBtn = $("#search-btn");
+  var clearBtn = $("#clear-btn");
+
+  searchBtn.on("click", function (event) {
+    event.preventDefault();
+    var entryValue = inputValue.val();
+    // console.log("clicked submit");
+    // console.log(inputValue.val());
+    var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${entryValue}&api-key=${apiKey}`;
+  });
+});
